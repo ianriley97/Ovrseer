391d1b734cfa12353f3503c9c8b496324b833af7
Dayton Taylor <daytonltaylor@outlook.com>
2017-12-18 20:34:22 -0600

    removed console logs

--- objects/inheritance/group.js
+++ objects/inheritance/group.js
@@ -23,8 +23,6 @@ class Group {
   };
   AddMember(id, member) {
     var m = this.Members.Add(id, new Member(member));
-    console.log(id);
-    console.log(m);
     Log[this.Identity](`Member, "${m.Object.username}", has joined ${this.Identity}, "${this.Name}".`);
     return m;
   };
