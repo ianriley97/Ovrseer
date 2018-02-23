b33a21c32dec6d0ada6e1a5c86e16fff0ce85350
Dayton Taylor <daytonltaylor@outlook.com>
2018-02-23 14:48:04 -0600

    removed server files to start fresh on dynamic server

--- bot.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-require('./server/server.js');
-require('./utility/commandLoader.js')();
-
-module.exports = {
-  DiscordApp: new (require('./objects/client/Discord/client.js'))(),
-  TwitchApp: new (require('./objects/client/Twitch/client.js'))()
-}
--- server/server.js
+++ server/server.js
@@ -1,49 +1,16 @@
 const http = require('http');
-const Express = require('express');
-const App = Express();
+const fs = require('fs');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 4040;
+const PORT = process.env.PORT || 80;
 
-// set the view engine to ejs
-App.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-App.use(Express.static(__dirname + '/public'));
-
-// set the home page route
-App.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-App.listen(port, function() {
-    console.log('App is running on http://localhost:' + port);
-});
-
-
-function startKeepAlive() {
-  setInterval(function() {
-    var options = {
-      host: 'ovrseer.herokuapp.com',
-      port: 80,
-      path: '/'
-    };
-    http.get(options, function(res) {
-      res.on('data', function(chunk) {
-        try {
-          // optional logging...disable after its working
-        //   console.log('HEROKU RESPONSE: ' + chunk);
-        }
-        catch (err) {
-          console.log(err.message);
-        }
-      });
-    }).on('error', function(err) {
-      console.log('Error: ' + err.message);
-    });
-  }, (20 * 60 * 1000)); // load every 20 minutes
+function handleRequest(req, res) {
+  
 }
-startKeepAlive();
+
+// Create the webserver
+var server = http.createServer(handleRequest);
+
+// Start listening for HTTP requests
+server.listen(PORT, function() {
+  console.log("Listening at port ", PORT);
+});
