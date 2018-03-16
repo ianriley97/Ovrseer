4644f9b97dcf34356364cd170f5738f6260cbd72
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-15 22:54:19 -0500

    added redirect to bot separate server url

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const Path = require('path');
 const HTTP = require('http');
 // Allow use of environment variables
 require('dotenv').config();
-// Initialize necessary objects for bot use
-const CommandList = new (require(Path.join(__dirname, 'collections', 'commands.js')))(Path.join(__dirname, 'commands'));
-// Initialize clients
-const DiscordClient = new (require(Path.join(__dirname, 'objects', 'discord', 'discord.js')))(CommandList);
-
+// Start server to attach running port and redirect to server url
 var server = HTTP.createServer(function(req, res) {
-  res.writeHead(301, {Location: 'https://ovrseer.herokuapp.com'});
+  res.writeHead(301, {Location: process.env.SERVER_REDIRECT_URL});
   res.end();
 });
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, function() {
   console.log('Bot running on port ' + PORT);
 });
+// Initialize necessary objects for bot use
+const CommandList = new (require(Path.join(__dirname, 'collections', 'commands.js')))(Path.join(__dirname, 'commands'));
+// Initialize clients
+const DiscordClient = new (require(Path.join(__dirname, 'objects', 'discord', 'discord.js')))(CommandList);
