239806f689516a05095c2d6178e9bdba712ec07d
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-15 20:50:53 -0500

    moving to separate server and bot

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Path = require('path');
 // Allow use of environment variables
 require('dotenv').config();
 // Initialize necessary objects for bot use
-require(Path.join(__dirname, 'server', 'server.js'));
 const CommandList = new (require(Path.join(__dirname, 'collections', 'commands.js')))(Path.join(__dirname, 'commands'));
 // Initialize clients
 const DiscordClient = new (require(Path.join(__dirname, 'objects', 'discord', 'discord.js')))(CommandList);
--- server/controllers/page-renderer.js
+++ server/controllers/page-renderer.js
@@ -4,10 +4,10 @@ const Path = require('path');
 const ViewDir = Path.join(__dirname, '..', 'views');
 
 function render(page, cb) {
-  FileSystem.readFile(Path.join(ViewDir, page, (page + '.html')), function(err, data) {
+  FileSystem.readFile(, function(err, dir) {
     if(err) cb(err);
     else {
-      cb(err, data);
+
     }
   });
 }
