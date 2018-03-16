c2790f4e0a8af4d34d6625f0d4ce6345c111dd47
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-15 22:20:23 -0500

    added http listener to allow bot to take requests

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const Path = require('path');
+const HTTP = require('http');
 // Allow use of environment variables
 require('dotenv').config();
 // Initialize necessary objects for bot use
 const CommandList = new (require(Path.join(__dirname, 'collections', 'commands.js')))(Path.join(__dirname, 'commands'));
 // Initialize clients
 const DiscordClient = new (require(Path.join(__dirname, 'objects', 'discord', 'discord.js')))(CommandList);
+
+var server = HTTP.createServer(function(req, res) {});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function() {
+  console.log('Bot running on port ' + PORT);
+});
