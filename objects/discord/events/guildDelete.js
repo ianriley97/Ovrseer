701741bd13be859bdee6fe86ee55894bc829b367
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-20 00:10:38 -0500

    added events for guilds and members

---
+++ objects/discord/events/guildDelete.js
@@ -0,0 +1,4 @@
+module.exports = function(app, args) { // args = [guild]
+  var client = app.Client;
+  var guild = args[0];
+};
---
+++ objects/discord/events/guildMemberRemove.js
@@ -0,0 +1,5 @@
+module.exports = function(app, args) { // args = [member]
+  var client = app.Client;
+  var member = args[0];
+
+};
