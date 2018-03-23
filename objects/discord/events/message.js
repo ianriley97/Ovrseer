c1c8f0952ce2098ec511e0bb67ae6cd4015bbc28
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-23 00:57:29 -0500

    exits execution if message is from direct message

--- objects/discord/events/message.js
+++ objects/discord/events/message.js
@@ -2,17 +2,19 @@ module.exports = function(app, args) { // args = [message]
   var client = app.client;
   var message = args[0];
   if(message.author.bot) return;
-  app.getGuild(message.guild, function(guild) {
-    app.getUser(message.author, function(member) {
+  var channel = message.channel;
+  if(channel.type == 'dm') return;
+  app.getUser(message.author, function(user) {
+    app.getGuild(message.guild, function(guild) {
       var cmd = app.checkForCmd(message.content, guild.cmd_prefix);
       if(cmd) {
         cmd.app = app,
         cmd.message = message;
         cmd.guild = guild;
-        cmd.user = member;
+        cmd.user = user;
         app.runCmd(cmd);
       }
       else app.parseMessage(message.content, guild.blacklist);
-    }, guild);
+    }, user);
   });
 };
