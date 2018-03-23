ea158703eb3873d32e9e62c37cc758bce4d0fc6d
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-23 01:54:20 -0500

    now creates guilds and/or members and notifies them of the bots intent as necessary

--- objects/discord/events/guildCreate.js
+++ objects/discord/events/guildCreate.js
@@ -1,4 +1,15 @@
 module.exports = function(app, args) { // args = [guild]
   var client = app.client;
   var guild = args[0];
+  app.addGuild(guild);
+  var channels = guild.channels;
+  channels.forEach(function(ch) {
+    if(ch.type == 'text') {
+      ch.send(`
+        Hello @everyone , and thanks for letting me join your guild!\n
+        **-This is just a notice-**\n
+        ${app.settings.app_purpose_notice_msg}
+      `);
+    }
+  });
 };
--- objects/discord/events/guildMemberAdd.js
+++ objects/discord/events/guildMemberAdd.js
@@ -1,4 +1,13 @@
 module.exports = function(app, args) { // args = [member]
   var client = app.client;
   var member = args[0];
+  app.getUser(member, function(user) {
+    app.getGuild(message.guild, function(guild) {
+      member.send(`
+        Hello and Welcome to "${guild.name}"!\n
+        **-This is just a notice-**\n
+        ${app.settings.app_purpose_notice_msg}
+      `);
+    }, user);
+  });
 };
