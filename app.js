8ca072729effca44f74a11995a04f0155a588fd2
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-23 01:22:42 -0500

    settings now includes the webpage url

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const Path = require('path');
 // Allow use of environment variables
 require('dotenv').config();
+// Initialize necessary objects
+const Settings = require(Path.join(__dirname, 'settings.json'));
 // Initialize server to allow Heroku connections and requests
 const HTTP = require('http');
 var server = HTTP.createServer(function(req, res) {
-  res.writeHead(301, {Location: 'https://ovrseer.herokuapp.com'});
+  res.writeHead(301, {Location: Settings.webpage});
   res.end();
 });
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, function() {
   console.log('Bot running on port ' + PORT);
 });
-// Initialize necessary objects
-const Settings = require(Path.join(__dirname, 'settings.json'));
 const CommandList = new (require(Path.join(__dirname, 'collections', 'commands.js')))(Path.join(__dirname, 'commands'), Settings.cmd_prefix);
 const WordParser = require(Path.join(__dirname, 'utilities', 'parse-string', 'word-parser.js'));
 const DBManager = new (require(Path.join(__dirname, 'database.js')))(null, function(err, dbManager) {
