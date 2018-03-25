94d8c696c112227456ce29b7737104adc83405be
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-25 02:42:28 -0500

    made into class module

---
+++ model/database.js
@@ -0,0 +1,27 @@
+class DBManager {
+  constructor(connection, cb, settings) {
+    this.settings = settings;
+    const {Client} = require('pg');
+    this.db = new Client({
+      connectionString: connection,
+      ssl: true
+    });
+    var dbm = this;
+    this.db.connect(function(err) {
+      if(err) {
+        dbm = null;
+        console.error(err);
+      }
+      else console.log('Database connection successful');
+      cb(err, dbm);
+    });
+  }
+  query(queryStr, cb) {
+    var dbObj = this;
+    this.db.query(queryStr).then(function(res) {
+      if(cb) cb(res, dbObj);
+    }).catch(function(err) { console.error(err); });
+  }
+}
+
+module.exports = DBManager;
