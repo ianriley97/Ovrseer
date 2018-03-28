de4907323d45225d949fdcbb96fb0ef1a9a79486
Dayton Taylor <daytonltaylor@outlook.com>
2018-03-28 01:39:40 -0500

    added rest request for discord client info

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const Path = require('path');
 require('dotenv').config();
 // Initialize necessary objects
 const Settings = require(Path.join(__dirname, 'settings.json'));
-// Initialize server listener to allow stable port connection on Heroku
-new (require(Path.join(__dirname, 'server.js')))((process.env.PORT || 3000), Settings);
 const CommandList = new (require(Path.join(__dirname, 'collections', 'commands.js')))(Path.join(__dirname, 'commands'), Settings.cmd_prefix);
 const WordParser = require(Path.join(__dirname, 'utilities', 'word-parser.js'));
 new (require(Path.join(__dirname, 'objects', 'database-manager.js')))(process.env.DATABASE_URL, function(err, dbManager) {
   const UserManager = new (require(Path.join(__dirname, 'objects', 'user-manager.js')))(dbManager, Settings);
-  new (require(Path.join(__dirname, 'objects', 'discord', 'discord.js')))(process.env.DISCORD_BOT_TOKEN, Settings, CommandList, dbManager, UserManager, WordParser);
+  const DiscordApp = new (require(Path.join(__dirname, 'objects', 'discord', 'discord.js')))(process.env.DISCORD_BOT_TOKEN, Settings, CommandList, dbManager, UserManager, WordParser);
+  // Initialize server listener to allow stable port connection on Heroku
+  new (require(Path.join(__dirname, 'server.js')))((process.env.PORT || 3000), Settings, dbManager, DiscordApp.client);
 });
--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const HTTP = require('http');
 
+var settings;
+var dbManager;
+var discordClient;
+
 class Server {
-  constructor(port, settings) {
-    this.settings = settings;
-    this.server = HTTP.createServer(this.handRequest);
+  constructor(port, s, dbm, dc) {
+    settings = s;
+    dbManager = dbm;
+    discordClient = dc;
+    discordClient.token = null;
+    ReqObjs.set('client', discordClient);
+    this.server = HTTP.createServer(handleRequest);
     this.server.listen(port, function() {
       console.log('App running on port ' + port);
     });
   }
-  handRequest(req, res) {
-    res.writeHead(301, {Location: this.settings.webpage});
+}
+
+module.exports = Server;
+
+function handleRequest(req, res) {
+  if(req.url.startsWith('/request')) serve(req, res);
+  else {
+    res.writeHead(301, {Location: settings.webpage});
     res.end();
   }
 }
 
-module.exports = Server;
+const ReqObjs = new Map();
+
+function serve(req, res) {
+  var url = req.url.split('/').slice(2);
+  res.writeHead(200, {"Content-Type": "application/json"});
+  var obj = ReqObjs.get(url[0]);
+  res.end(dbManager.convertFromObj(obj));
+}
